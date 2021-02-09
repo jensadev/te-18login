fc6469aaf4a0e514d353156dc900bca8f868a1ba
jensnti <jens.andreasson@ga.ntig.se>
2021-02-09 11:08:28 +0100

    feat(login.js): ultrasecret login

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET login form */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
   res.render('login', {title: 'Schoolsoft'});
 });
+
+/* POST login */
+router.post('/', function(req, res, next) {
+
+  console.log(req.body);
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (password == "Secret123") {
+    res.send('Topsecret!');
+  } else {
+    res.redirect('/login');
+  }
+
+
+});
+
 
 module.exports = router;
