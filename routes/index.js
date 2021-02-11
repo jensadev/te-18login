9a4be43247085c190a5c193cd5f35acb36c57328
jensnti <jens.andreasson@ga.ntig.se>
2021-02-11 09:35:21 +0100

    feat: adds express-session
    refactor: login route to use session
    
    feat: adds topsecret route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET topsekret page. */
+router.get('/topsecret', function(req, res, next) {
+  if (req.session.loggedin) {
+    res.send('Du är inloggad.');
+  } else {
+      res.send('Please login to view this page!');
+  }
+});
+
 module.exports = router;
